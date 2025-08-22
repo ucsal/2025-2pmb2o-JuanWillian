35345474290baca55dd8b68edd29b8dcd6a6c0f0
JuanWillian <juanwcaraujo28@gmail.com>
2025-08-22 15:02:27 -0300

    feat: criando a interface e suas classes implementadas

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,6 +1,11 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DiscountCalculator {
+    Map<CustomerType,DiscountPolicy> mapa = new HashMap<>();
+
     public double apply(double amount, CustomerType type){
         switch (type){
             case REGULAR: return amount * 0.95;
