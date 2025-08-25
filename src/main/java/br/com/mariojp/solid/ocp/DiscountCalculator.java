4eb938f043e84907b771aa3a2bcd2551de1400d9
JuanWillian <juanwcaraujo28@gmail.com>
2025-08-25 14:33:21 -0300

    refactor: modificando a discountCalculator para usar um map

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -4,13 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DiscountCalculator {
-    Map<CustomerType,DiscountPolicy> mapa = new HashMap<>();
+    Map<CustomerType, DiscountPolicy> map;
 
-    public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
-        }
+    public DiscountCalculator(Map<CustomerType, DiscountPolicy> map) {
+        this.map = map;
+    }
+
+    public DiscountCalculator() {
+        this.map = new HashMap<>();
+        map.put(CustomerType.PARTNER, new PartnerPolicy());
+        map.put(CustomerType.REGULAR, new RegularPolicy());
+        map.put(CustomerType.PREMIUM, new PremiumPolicy());
+    }
+
+    public double apply(double amount, CustomerType type) {
+        return map.get(type).apply(amount);
+    }
+
+    public void setMap(Map<CustomerType, DiscountPolicy> map) {
+        this.map = map;
     }
 }
